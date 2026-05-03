8aac142d98d35bb0ef8f4b91869efa2280952a9e
umayer10-AI <mdumayer10@gmail.com>
2026-05-03 20:03:09 +0600

    add

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -1,14 +1,19 @@
+"use client"
+import { Data } from '@/context/Context';
 import { Label, SearchField } from '@heroui/react';
-import React from 'react';
+import React, { useContext } from 'react';
 
 const SearchBar = () => {
+
+    const {s,setS} = useContext(Data)
+
     return (
         <div className='mb-5'>
             <SearchField name="search">
                 <Label>Search</Label>
                 <SearchField.Group className={'shadow-md shadow-cyan-500'}>
                     <SearchField.SearchIcon />
-                    <SearchField.Input className="w-70" placeholder="Search..." />
+                    <SearchField.Input onChange={(e) => setS(e.target.value)} className="w-70" placeholder="Search..." />
                     <SearchField.ClearButton />
                 </SearchField.Group>
             </SearchField>
