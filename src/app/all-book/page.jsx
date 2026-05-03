8df275f8575fcf84e44a51b041048a6cf3997db6
umayer10-AI <mdumayer10@gmail.com>
2026-05-03 19:49:45 +0600

    add

--- src/app/all-book/page.jsx
+++ src/app/all-book/page.jsx
@@ -1,15 +1,16 @@
 import Cards from '@/component/Cards';
+import SearchBar from '@/component/SearchBar';
 import { getPost } from '@/lib/fetching';
 import React from 'react';
 
 const page = async () => {
 
     const data = await getPost()
-    // console.log(data)
 
     return (
         <div>
             <h2 className='text-2xl font-bold my-5 bg-linear-to-r from-cyan-500 to-blue-600 text-transparent bg-clip-text'>All Books</h2>
+            <SearchBar></SearchBar>
             <Cards data={data}></Cards>
         </div>
     );
