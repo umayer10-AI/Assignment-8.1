124b5b843dcf28ec040f7e6ee7a9dae4297cb211
umayer10-AI <mdumayer10@gmail.com>
2026-05-03 12:15:31 +0600

    add

---
+++ src/lib/auth.js
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+const client = new MongoClient(process.env.MY);
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    client
+  }),
+});
